d9b6b84cbb626fee9866cafca0b73cc22a2cd9c7
armin <armin.zirak97@gmail.com>
2021-12-03 13:38:02 -0700

    curriculum learning automated bash

---
+++ add_bunch_of_data.py
@@ -0,0 +1,493 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+#
+# get_ipython().run_line_magic('load_ext', 'autoreload')
+# get_ipython().run_line_magic('autoreload', '2')
+
+
+# In[3]:
+
+
+from IPython.display import clear_output
+
+
+# In[4]:
+
+
+from transformers import Seq2SeqTrainer
+from transformers import Seq2SeqTrainingArguments
+from transformers import T5Config
+from transformers import T5ForConditionalGeneration
+from transformers import T5Tokenizer
+from transformers import set_seed
+
+
+# In[5]:
+
+
+from datetime import datetime
+import argparse
+import os
+import sys
+
+sys.path.append("./hf_transformers/")
+
+
+# In[6]:
+
+
+import torch
+
+from data_reader import GetDataAsPython
+from prepare_data import create_data
+from prepare_data import create_dataset
+from prepare_data import extract_warning_types
+from utils import boolean_string
+from utils import get_current_time
+import csv
+import numpy as np
+
+
+# In[7]:
+
+local = True
+
+if local:
+    storage_directory = './storage/'
+    batch_size = 16
+else:
+    storage_directory = '/scratch/arminz/'
+    batch_size = 64
+
+# In[8]:
+
+
+import random
+
+
+# In[9]:
+
+
+exec_number = random.randint(0, 1000)
+exec_number
+
+
+# In[10]:
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--append", type=int, required=True)
+parser.add_argument("-rp", "--repo_percent", type=float, required=True)
+parser.add_argument("-r", "--repo", type=str, required=True)
+
+args = parser.parse_args()
+append = args.append
+repo = args.repo
+repo_percent = args.repo_percent
+
+
+# repo = '/data/all/data/appium/appium'
+# repo
+
+
+# In[11]:
+
+
+# repo_percent = 0.0
+print('repo percent:', repo_percent)
+
+
+# In[12]:
+
+
+name='added'
+name
+
+
+# In[13]:
+
+
+# Read and prepare data
+data = GetDataAsPython(f"{storage_directory}/data_and_models/data/data_autofix_tracking_repo_specific_final.json")
+data_eslint = GetDataAsPython(f"{storage_directory}/data_and_models/data/data_autofix_tracking_eslint_final.json")
+data += data_eslint
+
+
+# In[14]:
+
+
+len(data)
+
+
+# In[15]:
+
+
+all_warning_types = extract_warning_types(data)
+
+
+# In[16]:
+
+
+(repo_train_inputs, repo_train_labels, repo_val_inputs, repo_val_labels, repo_test_inputs, repo_test_labels, repo_train_info, repo_val_info, repo_test_info, ) =    create_data(data, all_warning_types, include_warning=True, design='repo-based-included', select_repo=repo)
+
+
+# In[17]:
+
+
+(general_train_inputs, general_train_labels, general_val_inputs, general_val_labels, general_test_inputs, general_test_labels, general_train_info, general_val_info, general_test_info, ) =    create_data(data, all_warning_types, include_warning=True, design='repo-based')
+
+
+# In[18]:
+
+
+from transformers import AutoTokenizer, AutoModel
+
+code_bert_model = AutoModel.from_pretrained("microsoft/codebert-base")
+code_bert_model.to('cuda')
+
+code_bert_tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
+
+
+# In[19]:
+
+
+def code_to_vec(code): # probably need a normalization
+    code_tokens=code_bert_tokenizer.tokenize(code)
+    tokens=[code_bert_tokenizer.cls_token]+code_tokens+[code_bert_tokenizer.sep_token]
+    tokens_ids=code_bert_tokenizer.convert_tokens_to_ids(tokens)
+    context_embeddings=code_bert_model(torch.tensor(tokens_ids).to('cuda')[None,:])[0]
+    return context_embeddings[0][0].cpu().detach().numpy()
+def vec_distance(code1, code2):
+    n_code1 = code1/np.linalg.norm(code1)
+    n_code2 = code1/np.linalg.norm(code2)
+    return np.linalg.norm(n_code1 - n_code2)
+
+
+# In[20]:
+
+
+repo_vecs = np.array([code_to_vec(item) for item in repo_train_inputs])
+
+
+# In[21]:
+
+
+repo_vecs.shape
+
+
+# In[22]:
+
+
+from sklearn.neighbors import NearestNeighbors
+
+
+# In[23]:
+
+
+# repo_center = np.average(repo_vecs, axis=0)
+
+
+# In[24]:
+
+
+# %%time
+# general_vecs = np.array([code_to_vec(item[:512]) for item in general_train_inputs[:]])
+
+
+# In[25]:
+
+
+# general_vecs.shape
+
+
+# In[26]:
+
+
+# np.save('general_arr_all.npy', general_vecs)
+
+
+# In[27]:
+
+
+general_vecs = np.load('general_arr_all.npy')
+general_vecs.shape
+
+
+# In[28]:
+
+
+nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(repo_vecs)
+
+
+# In[29]:
+
+
+distances, indices = nbrs.kneighbors(general_vecs)
+
+
+# In[30]:
+
+
+# diffs = [vec_distance(general_vec, repo_center) for general_vec in general_vecs]
+
+
+# In[31]:
+
+
+import matplotlib.pyplot as plt
+plt.subplot(2, 1, 1)
+plt.boxplot(distances)
+plt.subplot(2, 1, 2)
+plt.hist(distances)
+
+
+# In[32]:
+
+
+repo_nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(repo_vecs)
+repo_distances, index = repo_nbrs.kneighbors(repo_vecs)
+repo_distances.shape
+
+
+# In[33]:
+
+
+plt.hist(repo_distances[:,1])
+
+
+# In[34]:
+
+
+# top_n = 597#int(len(diffs) / 10)
+# top_n
+
+
+# In[38]:
+threshold = sorted(distances)[append]
+print('threshold:', threshold, 'append:', append)
+
+selected = (distances < threshold)
+# selected.sum()
+
+
+# In[39]:
+
+
+# selected_ind = np.argpartition(diffs, top_n)[:top_n]
+
+
+# In[40]:
+
+
+# np.array(diffs)[selected_ind].mean(), np.array(diffs).mean()
+
+
+# In[41]:
+
+
+added_inputs = list()
+added_labels = list()
+added_info = list()
+for ind in range(len(general_train_inputs)):
+    if selected[ind]:
+        added_inputs.append(general_train_inputs[ind])
+        added_labels.append(general_train_labels[ind])
+        added_info.append(general_train_info[ind])
+
+
+# In[42]:
+
+
+# Create dataset required by pytorch
+samples = int(repo_percent * len(repo_train_inputs))
+print(f'{len(repo_train_inputs)} {samples} {repo_percent}')
+
+
+# In[43]:
+
+
+len(repo_train_inputs), len(added_inputs)
+
+
+# In[44]:
+
+
+added_inputs += repo_train_inputs[:samples]
+added_labels += repo_train_labels[:samples]
+added_info += repo_train_info[:samples]
+
+
+# In[45]:
+
+
+validation_point = int((len(added_inputs) * 4) / 5)
+
+
+# In[46]:
+
+
+len(added_inputs)
+
+
+# In[47]:
+
+
+load_model = f'./{storage_directory}/checkpoint-37375'
+tokenizer = T5Tokenizer.from_pretrained(load_model)
+
+
+# In[48]:
+
+
+train_dataset = create_dataset(added_inputs[:validation_point], added_labels[:validation_point], tokenizer, pad_truncate=True, max_length=128)
+val_dataset = create_dataset(added_inputs[validation_point:], added_labels[validation_point:], tokenizer, pad_truncate=True)
+test_dataset = create_dataset(repo_val_inputs, repo_val_labels, tokenizer, pad_truncate=True)
+
+
+# In[49]:
+
+
+now = datetime.now()
+test_result_directory = f'{storage_directory}/fine-tune-result'
+full_name = f'{name}_{exec_number}_{repo.rsplit("/", 1)[1][-20:]}_{repo_percent}_{selected.sum()}'
+model_directory = f'{storage_directory}/tmp/{full_name}'
+model_directory
+
+
+# In[50]:
+
+
+lr = 4e-3
+ws = 300
+wd = 0.4
+lr, wd, ws
+
+
+# In[51]:
+
+
+tokenizer = T5Tokenizer.from_pretrained(load_model)
+model = T5ForConditionalGeneration.from_pretrained(load_model)
+model.resize_token_embeddings(len(tokenizer))
+# model.eval()
+
+
+# In[52]:
+
+
+from transformers import EarlyStoppingCallback
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir=model_directory,
+    num_train_epochs=15,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    warmup_steps=ws,
+    weight_decay=wd,
+    logging_dir=model_directory,
+    logging_steps=100,
+    do_eval=True,
+    evaluation_strategy="epoch",
+    learning_rate=lr,
+    load_best_model_at_end=True,
+    metric_for_best_model="eval_loss",
+    greater_is_better=False,
+    save_total_limit=1,
+    eval_accumulation_steps=1,  # set this lower, if testing or validation crashes
+    disable_tqdm=False,
+    predict_with_generate=True,  # never set this to false.
+    seed=42,  # default value
+)
+
+
+# In[53]:
+
+
+# from sklearn.metrics import accuracy_score
+# import numpy as np
+# def compute_metrics(p):
+#     target_max_length = 256
+#     predictions, labels = p.predictions, p.label_ids
+    
+#     predictions = np.pad(predictions, ((0, 0), (0, target_max_length - predictions.shape[1])), mode="constant")
+#     predictions = np.delete(predictions, 0, axis=1)
+#     predictions = np.insert(predictions, target_max_length - 1, 0, axis=1)
+
+#     labels = np.array(labels)
+#     labels = np.pad(labels, ((0, 0), (0, target_max_length - labels.shape[1])), mode="constant")
+#     labels = np.delete(labels, 0, axis=1)
+#     labels = np.insert(labels, target_max_length - 1, 0, axis=1)
+
+#     correct_counter = np.sum(np.all(np.equal(labels, predictions), axis=1))
+#     return {'acc': int(correct_counter)}
+
+
+# In[54]:
+
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    optimizers=[torch.optim.Adam(params=model.parameters(), lr=lr), None],
+    tokenizer=tokenizer,
+    callbacks = [EarlyStoppingCallback(early_stopping_patience=5)]
+#     compute_metrics=compute_metrics
+)
+
+
+# In[55]:
+
+
+trainer.train()
+
+
+# In[56]:
+
+
+trainer.evaluate()['eval_loss']
+
+
+# In[57]:
+
+
+trainer.predict(test_dataset).metrics
+
+
+# In[58]:
+
+
+best_model_dir = f'{model_directory}/best'
+trainer.save_model(best_model_dir)
+print('best model dir', best_model_dir)
+
+
+# In[59]:
+
+
+# del train_dataset
+# del test_dataset
+# del val_dataset
+# del trainer
+
+
+# In[60]:
+
+
+from numba import cuda 
+device = cuda.get_current_device()
+device.reset()
+
+
+# In[61]:
+
+
+os.system(f'python hf_transformers/tfix_testing.py --load-model {best_model_dir} -bs {batch_size} --model-name t5-small -d repo-based-included -r {repo}')
+
+import shutil
+shutil.rmtree(best_model_dir)
+
+
---
+++ curricular_learning.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --gres=gpu:v100:1
+#SBATCH --job-name=cl # single job name for the array
+#SBATCH --time=20:00:00 # maximum walltime per job
+#SBATCH --mem=40G # maximum 100M per job
+#SBATCH --cpus-per-task=2
+#SBATCH --output=cl.out # standard output
+#SBATCH --error=cl.err # standard error
+# in the previous two lines %A" is replaced by job
+
+source env/bin/activate
+
+for repo in /data/all/data/appium/appium /data/all/data/girder/girder /data/all/data/oroinc/platform /data/all/data/svgdotjs/svg.js /data/all/data/wsick/Fayde /data/all/data/appium/appium /data/all/data/zloirock/core-js /data/all/data/Vincit/objection.js /data/all/data/request/request /data/all/data/qooxdoo/qooxdoo
+do
+  for repo_percent in 0.0 0.1 0.2 0.3
+  do
+    for append in $(seq 50 100 1000)
+    do
+      echo $repo $repo_percent $append
+      python add_bunch_of_data.py -a $append -r $repo -rp $repo_percent
+    done
+  done
+done
