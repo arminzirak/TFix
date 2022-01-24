cf561bacebda86db4ec16198dc098a89292df5dc
armin <armin.zirak97@gmail.com>
2022-01-24 13:20:49 -0700

    test scripts

--- tester.sh
+++ tester.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --job-name=test # single job name for the array
-#SBATCH --time=2:00:00 # maximum walltime per job
-#SBATCH --mem=40G # maximum 100M per job
-#SBATCH --cpus-per-task=2
-#SBATCH --output=test.out # standard output
-#SBATCH --error=test.err # standard error
+#SBATCH --time=0:40:00 # maximum walltime per job
+#SBATCH --mem=15 # maximum 100M per job
+#SBATCH --cpus-per-task=1
+#SBATCH --output=%x.out # standard output
+#SBATCH --error=%x.err # standard error
 # in the previous two lines %A" is replaced by job
 
 cd ~/TFix
 source env/bin/activate
-python hf_transformers/tfix_testing.py --load-model ./storage/checkpoint-37375 -bs 16 --model-name t5-small -d repo-based-included #-r /data/all/data/appium/appium
+python hf_transformers/tfix_testing.py --load-model [] -bs 16 --model-name t5-small -d repo-based-included #-r /data/all/data/appium/appium
---
+++ tester_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --job-name=test-smA # single job name for the array
+#SBATCH --time=4:00:00 # maximum walltime per job
+#SBATCH --mem=15G # maximum 100M per job
+#SBATCH --cpus-per-task=1
+#SBATCH --output=%x.out # standard output
+#SBATCH --error=%x.err # standard error
+# in the previous two lines %A" is replaced by job
+
+cd ~/TFix
+source env/bin/activate
+for repo in /data/all/data/qooxdoo/qooxdoo /data/all/data/elastic/kibana /data/all/data/emberjs/ember.js /data/all/data/zloirock/core-js /data/all/data/Encapsule-Annex/onm /data/all/data/sequelize/sequelize /data/all/data/dcos/dcos-ui /data/all/data/LivelyKernel/LivelyKernel /data/all/data/svgdotjs/svg.js /data/all/data/foam-framework/foam
+do
+  echo $repo
+  python hf_transformers/tfix_testing.py --load-model [] -bs 128 --model-name t5-small -d repo-based-included -r $repo
+done
