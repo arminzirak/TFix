3226927e71454e81ced42aa98301df3ea6408f94
armin <armin.zirak97@gmail.com>
2022-01-25 18:35:26 -0700

    scripts fix

--- tester_all.sh
+++ tester_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:v100l:1
-#SBATCH --job-name=test-smA # single job name for the array
-#SBATCH --time=4:00:00 # maximum walltime per job
+#SBATCH --job-name=test-smiA # single job name for the array
+#SBATCH --time=3:00:00 # maximum walltime per job
 #SBATCH --mem=15G # maximum 100M per job
 #SBATCH --cpus-per-task=1
 #SBATCH --output=%x.out # standard output
@@ -13,5 +13,5 @@ source env/bin/activate
 for repo in /data/all/data/qooxdoo/qooxdoo /data/all/data/elastic/kibana /data/all/data/emberjs/ember.js /data/all/data/zloirock/core-js /data/all/data/Encapsule-Annex/onm /data/all/data/sequelize/sequelize /data/all/data/dcos/dcos-ui /data/all/data/LivelyKernel/LivelyKernel /data/all/data/svgdotjs/svg.js /data/all/data/foam-framework/foam
 do
   echo $repo
-  python hf_transformers/tfix_testing.py --load-model [] -bs 128 --model-name t5-small -d repo-based-included -r $repo
+  python hf_transformers/tfix_testing.py --load-model t5-small_repo-based-included_21-01-2022_10-30-33/checkpoint-17370 -bs 128 --model-name t5-small -d repo-based-included -r $repo
 done
--- trainer.sh
+++ trainer.sh
@@ -2,10 +2,10 @@
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --job-name=train # single job name for the array
 #SBATCH --time=60:00:00 # maximum walltime per job
-#SBATCH --mem=40G # maximum 100M per job
+#SBATCH --mem=15G # maximum 100M per job
 #SBATCH --cpus-per-task=2
-#SBATCH --output=train.out # standard output
-#SBATCH --error=train.err # standard error
+#SBATCH --output=%x.out # standard output
+#SBATCH --error=%x.err # standard error
 # in the previous two lines %A" is replaced by job
 
 #large can't do 16
