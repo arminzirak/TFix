e17be8ca5bfa6905be220be39fc48115c765eff7
sepehrhashtroudi@gamil.com <sepehrhashtroudi@gmail.com>
2022-05-03 19:19:25 -0600

    fix test leakage on catastrophic forgetting (source-test)

--- hf_transformers/prepare_data.py
+++ hf_transformers/prepare_data.py
@@ -33,7 +33,8 @@ def filter_rule(data: List[DataPoint], rule_type: str) -> List[DataPoint]:
     return filtered_data
 
 
-def split_filtered(filtered_data: List[DataPoint], include_warning: bool, design: str, select_repo=None, back_translation=False,seed=13, no_split=False, no_valid=False):
+def split_filtered(filtered_data: List[DataPoint], include_warning: bool, design: str, select_repo=None,
+                   back_translation=False, seed=13, no_split=False, no_valid=False):
     filtered_data_temp = filtered_data
 
     if not back_translation:
@@ -42,7 +43,6 @@ def split_filtered(filtered_data: List[DataPoint], include_warning: bool, design
     else:
         inputs = [data_point.GetTBugRepresentation(include_warning)[0] for data_point in filtered_data]
         outputs = [data_point.GetTBugRepresentation(include_warning)[1] for data_point in filtered_data_temp]
-
 
     if design == 'new':
         raise Exception('deprecated design')
@@ -69,7 +69,7 @@ def split_filtered(filtered_data: List[DataPoint], include_warning: bool, design
         #     inputs, outputs, shuffle=True, random_state=seed, test_size=test_size
         # )
         # train_info, test_info = train_test_split(filtered_data, shuffle=True, random_state=seed, test_size=test_size)
-    elif design.startswith('repo-based'):
+    elif design.startswith('repo-based') or design.startswith('source-test'):
         repos = pd.read_csv('./repos_3.csv', index_col=0)
         target_repos = repos[repos['category'] == 'target']
 
@@ -87,11 +87,11 @@ def split_filtered(filtered_data: List[DataPoint], include_warning: bool, design
         train_inputs, train_labels, train_info = list(), list(), list()
         test_inputs, test_labels, test_info = list(), list(), list()
         for repo in input_repo:
-            if not (target_repos['repo'] == repo).any():
+            if not (target_repos['repo'] == repo).any():  # (source)
                 train_inputs += input_repo[repo]
                 train_labels += output_repo[repo]
                 train_info += filtered_instance_repo[repo]
-            else:
+            else:  # target
                 if len(input_repo[repo]) < 2:
                     train_inputs += input_repo[repo]
                     train_labels += output_repo[repo]
@@ -112,59 +112,23 @@ def split_filtered(filtered_data: List[DataPoint], include_warning: bool, design
                     train_inputs += input_repo[repo]
                     train_labels += output_repo[repo]
                     train_info += filtered_instance_repo[repo]
-    elif design.startswith('source-test'):
-        repos = pd.read_csv('./repos_3.csv', index_col=0)
-        target_repos = repos[repos['category'] == 'target']
-        assert select_repo is None
-
-        input_repo = defaultdict(list)
-        output_repo = defaultdict(list)
-        filtered_instance_repo = defaultdict(list)
-        for input_instance, output_instance, filtered_data_instance in zip(inputs, outputs, filtered_data):
-            this_repo = filtered_data_instance.repo
-            input_repo[this_repo].append(input_instance)
-            output_repo[this_repo].append(output_instance)
-            filtered_instance_repo[this_repo].append(filtered_data_instance)
-
-        train_inputs, train_labels, train_info = list(), list(), list()
-        test_inputs, test_labels, test_info = list(), list(), list()
-        random_remove = 0
-        for repo in input_repo:
-            if (target_repos['repo'] == repo).any():
-                continue
-            else:
-                if len(input_repo[repo]) < 2:
-                    train_inputs += input_repo[repo]
-                    train_labels += output_repo[repo]
-                    train_info += filtered_instance_repo[repo]
-                    continue
-                if len(input_repo[repo]) < 20:
-                    if random_remove % 3 == 4:
-                        continue
-                    else:
-                        random_remove += 1
-                this_train_input, this_test_input, this_train_output, this_test_output, this_train_fi, this_test_fi = \
-                    train_test_split(input_repo[repo], output_repo[repo], filtered_instance_repo[repo],
-                                     shuffle=True, random_state=seed, test_size=0.25)
-                test_inputs += this_test_input
-                test_labels += this_test_output
-                test_info += this_test_fi
-                train_inputs += this_train_input
-                train_labels += this_train_output
-                train_info += this_train_fi
     else:
         print(f'wrong design argument {design}')
         return
     if not no_valid and len(train_inputs) > 1:
         # print(
         #     f'train size: {len(train_inputs)} | test size: {len(test_inputs)} | ratio: {len(test_inputs) / (len(test_inputs) + len(train_inputs)):.2f}')
-        val_size = 0.25  # if len(train_inputs) >= 10 else 1 / len(train_inputs) #TODO: make it 0.20
+        val_size = 0.25  # if len(train_inputs) >= 10 else 1 / len(train_inputs)
         train_inputs, val_inputs, train_labels, val_labels = train_test_split(
             train_inputs, train_labels, shuffle=True, random_state=seed, test_size=val_size
         )
         train_info, val_info = train_test_split(train_info, shuffle=True, random_state=seed, test_size=val_size)
     else:
         val_inputs, val_labels, val_info = [], [], []
+
+    if design.startswith('source-test'):
+        _, test_inputs, _, test_labels, _, test_info = train_test_split(val_inputs, val_labels, val_info,
+                                                               random_state=seed, test_size=0.15)
 
     return (
         train_inputs,
@@ -197,7 +161,8 @@ def create_data(data: List[DataPoint], linter_warnings: List[str], include_warni
     for warning in linter_warnings:
         filtered_data = filter_rule(data, warning)
         (train_w, train_w_labels, val_w, val_w_labels, test_w, test_w_labels, train_w_info, val_w_info, test_w_info,) \
-            = split_filtered(filtered_data, include_warning, design, select_repo=select_repo, back_translation=back_translation, no_split=no_split)
+            = split_filtered(filtered_data, include_warning, design, select_repo=select_repo,
+                             back_translation=back_translation, no_split=no_split)
 
         train += train_w
         train_labels += train_w_labels
@@ -218,8 +183,9 @@ def create_data(data: List[DataPoint], linter_warnings: List[str], include_warni
           .format(len(train), len(val), n_test_samples))
     return train, train_labels, val, val_labels, test, test_labels, train_info, val_info, test_info
 
+
 def create_data_tbug(data: List[DataPoint], linter_warnings: List[str], include_warning: bool, design: str,
-                select_repo=None, back_translation=False, no_split=False):
+                     select_repo=None, back_translation=False, no_split=False):
     train: DefaultDict[str, List[str]] = defaultdict(list)
     train_labels: DefaultDict[str, List[str]] = defaultdict(list)
 
@@ -233,7 +199,8 @@ def create_data_tbug(data: List[DataPoint], linter_warnings: List[str], include_
     for warning in linter_warnings:
         filtered_data = filter_rule(data, warning)
         (train_w, train_w_labels, _, _, test_w, test_w_labels, train_w_info, _, test_w_info,) \
-            = split_filtered(filtered_data, include_warning, design, select_repo=select_repo, back_translation=back_translation, no_split=no_split, no_valid=True)
+            = split_filtered(filtered_data, include_warning, design, select_repo=select_repo,
+                             back_translation=back_translation, no_split=no_split, no_valid=True)
 
         train[warning] += train_w
         train_labels[warning] += train_w_labels
